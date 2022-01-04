3b1a8646a1cb04487d11061112e62c75a7d09d73
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-01-04 16:17:09 -0500

    add grid

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,15 @@
+import { Grid, GridItem } from '@chakra-ui/react';
+
 import Layout from '../components/layout';
 
 export default function Web() {
   return (
     <Layout>
-      <h1>Web</h1>
+      <Grid templateColumns='repeat(7, 1fr)'>
+        <GridItem bg='tomato' />
+        <GridItem bg='yellow' colSpan={5} />
+        <GridItem bg='aqua' />
+      </Grid>
     </Layout>
   );
 }
