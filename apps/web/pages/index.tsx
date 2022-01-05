69338328ca7a7b15c3bc2be9dd43eaa6dd2b6272
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-01-04 20:05:46 -0500

    add grid gap

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -6,7 +6,7 @@ import SideMenu from '../components/SideMenu';
 export default function Web() {
   return (
     <Layout>
-      <Grid templateColumns='repeat(7, 1fr)' mt={16}>
+      <Grid templateColumns='repeat(7, 1fr)' columnGap={4} mt={16}>
         <SideMenu />
         <GridItem bg='yellow' colSpan={5} />
         <GridItem bg='aqua' />
