a9a1a43bfe46f09735ea0a6dfbf4a28976622bdb
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-01-07 18:47:52 -0500

    show buttons or avatar

--- apps/web/components/Navbar/index.tsx
+++ apps/web/components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Flex,
   Center,
@@ -10,11 +11,62 @@ import {
   Menu,
   MenuButton,
   MenuList,
-  MenuItem
+  MenuItem,
+  Button,
+  Stack
 } from '@chakra-ui/react';
 import { BellIcon } from '@chakra-ui/icons';
 
 export default function Navbar() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  let showBtnsOrAvatar;
+
+  if (loggedIn) {
+    showBtnsOrAvatar = (
+      <>
+        <IconButton
+          aria-label='Your Notifications'
+          icon={<BellIcon w={6} h={6} />}
+          mr={5}
+          bg='white'
+        />
+        <Menu>
+          <MenuButton>
+            <Avatar size='xs' src='https://bit.ly/broken-link' />
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Profile</MenuItem>
+            <MenuItem>Settings</MenuItem>
+            <MenuItem>Logout</MenuItem>
+          </MenuList>
+        </Menu>
+      </>
+    );
+  } else {
+    showBtnsOrAvatar = (
+      <Stack direction='row' spacing={5}>
+        <Button
+          variant='outline'
+          borderColor='star-command-blue'
+          color='star-command-blue'
+          size='sm'
+        >
+          Login
+        </Button>
+        <Button
+          variant='solid'
+          bg='navy-blue'
+          color='white'
+          _hover={{ bg: 'star-command-blue' }}
+          size='sm'
+        >
+          Sign Up
+        </Button>
+      </Stack>
+    );
+  }
+
   return (
     <Container maxW='100%' bg='white' py={4} boxShadow='md'>
       <Flex maxW='1400px' mx='auto'>
@@ -33,24 +85,7 @@ export default function Navbar() {
           />
         </Center>
         <Spacer />
-        <Center>
-          <IconButton
-            aria-label='Your Notifications'
-            icon={<BellIcon w={6} h={6} />}
-            mr={5}
-            bg='white'
-          />
-          <Menu>
-            <MenuButton>
-              <Avatar size='xs' src='https://bit.ly/broken-link' />
-            </MenuButton>
-            <MenuList>
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Settings</MenuItem>
-              <MenuItem>Logout</MenuItem>
-            </MenuList>
-          </Menu>
-        </Center>
+        <Center>{showBtnsOrAvatar}</Center>
       </Flex>
     </Container>
   );
