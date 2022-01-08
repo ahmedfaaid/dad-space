78b22580b15255a1c4af7b031ff79590ab8fcd01
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-01-08 16:43:01 -0500

    add urql client

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,13 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { createClient, Provider } from 'urql';
 import '@fontsource/source-sans-pro/400.css';
 import '@fontsource/open-sans/700.css';
 import customTheme from 'ui/theme';
 
+const client = createClient({
+  url: 'http://localhost:5500/v1'
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={customTheme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <Provider value={client}>
+      <ChakraProvider theme={customTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
   );
 }
 
