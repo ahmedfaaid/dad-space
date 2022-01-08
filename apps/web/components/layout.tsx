5e4c98474b2cd6bdec57dfd31370ecbed4c92deb
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-01-07 19:42:41 -0500

    add head

--- apps/web/components/layout.tsx
+++ apps/web/components/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Container } from '@chakra-ui/react';
 
 import Navbar from './Navbar';
@@ -5,6 +6,9 @@ import Navbar from './Navbar';
 export default function Layout({ children }) {
   return (
     <>
+      <Head>
+        <title>Dad Space</title>
+      </Head>
       <div>
         <Navbar />
       </div>
