59e3c23809213cafab25405cb63fcccb260ad31a
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-01-18 22:33:39 -0500

    Login page

---
+++ apps/web/pages/login.tsx
@@ -0,0 +1,154 @@
+import { useState } from 'react';
+import NextLink from 'next/link';
+import { useRouter } from 'next/router';
+import {
+  Flex,
+  Stack,
+  Heading,
+  Box,
+  useColorModeValue,
+  Link,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Text,
+  useToast
+} from '@chakra-ui/react';
+import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import { useFormik } from 'formik';
+import { useLoginMutation } from 'dad-gql';
+import Layout from '../components/layout';
+import { loginSchema } from '../utils/formValidation';
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+  const toast = useToast();
+
+  const [, login] = useLoginMutation();
+  const { handleSubmit, handleChange, values, isSubmitting, errors } =
+    useFormik({
+      initialValues: {
+        email: '',
+        password: ''
+      },
+      validationSchema: loginSchema,
+      onSubmit: async (values, { setSubmitting, setErrors }) => {
+        const res = await login(values);
+
+        if (res.data?.login.errors) {
+          toast({
+            title: 'Login unsuccessful',
+            description: `There was an error logging in: ${res.data.login.errors[0].message}`,
+            status: 'error',
+            duration: 9000,
+            isClosable: true
+          });
+          setSubmitting(false);
+        } else if (res.data?.login.user) {
+          toast({
+            title: 'Login Successful',
+            description: 'You have successfully logged in!',
+            status: 'success',
+            duration: 9000,
+            isClosable: true
+          });
+          router.push('/');
+          setSubmitting(false);
+        }
+      }
+    });
+
+  return (
+    <Layout>
+      <Flex mt={4} align='center' justify='center'>
+        <Stack spacing={8} mx='auto' maxW='lg' py={12} px={6}>
+          <Stack align='center'>
+            <Heading fontSize='4xl' textAlign='center'>
+              Login
+            </Heading>
+            <Text fontSize='lg' color='gray.600'>
+              to start interacting with other dads ✌️
+            </Text>
+          </Stack>
+          <Box
+            rounded='lg'
+            bg={useColorModeValue('white', 'gray.700')}
+            boxShadow='lg'
+            p={8}
+          >
+            <form onSubmit={handleSubmit}>
+              <Stack spacing={4}>
+                <FormControl isRequired isInvalid={!!errors.email}>
+                  <FormLabel htmlFor='email'>Email address</FormLabel>
+                  <Input
+                    id='email'
+                    type='email'
+                    name='email'
+                    value={values.email}
+                    onChange={handleChange}
+                  />
+                  {errors.email ? (
+                    <FormErrorMessage>{errors.email}</FormErrorMessage>
+                  ) : null}
+                </FormControl>
+                <FormControl isRequired isInvalid={!!errors.password}>
+                  <FormLabel htmlFor='password'>Password</FormLabel>
+                  <InputGroup>
+                    <Input
+                      id='password'
+                      type={showPassword ? 'text' : 'password'}
+                      name='password'
+                      value={values.password}
+                      onChange={handleChange}
+                    />
+                    <InputRightElement h='full'>
+                      <Button
+                        variant='ghost'
+                        onClick={() =>
+                          setShowPassword(showPassword => !showPassword)
+                        }
+                      >
+                        {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                  {errors.password ? (
+                    <FormErrorMessage>{errors.password}</FormErrorMessage>
+                  ) : null}
+                </FormControl>
+                <Stack spacing={10} pt={2}>
+                  <Button
+                    loadingText='Submitting'
+                    size='lg'
+                    bg='star-command-blue'
+                    color='white'
+                    _hover={{
+                      bg: 'blue.500'
+                    }}
+                    isLoading={isSubmitting}
+                    type='submit'
+                  >
+                    Login
+                  </Button>
+                </Stack>
+                <Stack pt={6}>
+                  <Text align='center'>
+                    Not a registered user?{' '}
+                    <NextLink href='/signup' passHref>
+                      <Link color='star-command-blue'>Sign up</Link>
+                    </NextLink>
+                  </Text>
+                </Stack>
+              </Stack>
+            </form>
+          </Box>
+        </Stack>
+      </Flex>
+    </Layout>
+  );
+}
