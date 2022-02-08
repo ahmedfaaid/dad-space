515d51a1b7e1aea60f936d2087fadb1f5cda75d6
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-02-07 23:40:40 -0500

    basic comment card

--- apps/web/components/PostOnPage/Comment.tsx
+++ apps/web/components/PostOnPage/Comment.tsx
@@ -1,9 +1,40 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Avatar, Box, Flex, IconButton, Link, Text } from '@chakra-ui/react';
+import { Icon } from '@chakra-ui/icons';
+import { BsReply } from 'react-icons/bs';
+import { distanceInWordsToNow } from 'date-fns';
+import { Comment as CommentType } from '../../types';
 
-export default function Comment({ comment }) {
+interface CommentProps {
+  comment: CommentType;
+}
+
+export default function Comment({ comment }: CommentProps) {
   return (
-    <Box key={comment.id}>
-      <Text>{comment.text}</Text>
+    <Box>
+      <Flex align='center'>
+        <Avatar src='https://bit.ly/broken-link' w={5} h={5} />
+        <Text ml={2} fontSize='sm'>
+          <Link color='star-command-blue' fontSize='sm'>
+            {comment.postedBy.firstName} {comment.postedBy.lastName}
+          </Link>
+        </Text>
+        <Text ml={2} fontSize='sm'>
+          {distanceInWordsToNow(new Date(comment.createdAt))} ago
+        </Text>
+      </Flex>
+      <Text ml={7} fontSize='sm'>
+        {comment.text}
+      </Text>
+      <Flex align='center' ml={7} mt={2}>
+        <Link>
+          <Text fontSize='sm' fontWeight='semibold'>
+            <Icon as={BsReply} w={4} h={3} /> Reply
+          </Text>
+        </Link>
+        <Text fontSize='sm' fontWeight='semibold'>
+          <Link ml={2}>Report</Link>
+        </Text>
+      </Flex>
     </Box>
   );
 }
