e43f4ebc678fe478d6505c0338dbf1cb474b85f6
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-03-05 19:24:17 -0500

    update cache when comments are created or deleted

--- apps/web/utils/createUrqlClient.ts
+++ apps/web/utils/createUrqlClient.ts
@@ -1,7 +1,12 @@
 import { createClient, dedupExchange, fetchExchange } from 'urql';
 import { gql } from '@urql/core';
 import { cacheExchange } from '@urql/exchange-graphcache';
-import { VoteMutationVariables } from 'dad-gql';
+import {
+  CreateCommentMutationVariables,
+  VoteMutationVariables,
+  CommentsResponse,
+  DeleteCommentMutationVariables
+} from 'dad-gql';
 
 export const createUrqlClient = createClient({
   url: 'http://localhost:5500/v1',
@@ -11,6 +16,9 @@ export const createUrqlClient = createClient({
   exchanges: [
     dedupExchange,
     cacheExchange({
+      keys: {
+        PostResponse: () => null
+      },
       updates: {
         Mutation: {
           vote: (_result, args, cache, _info) => {
@@ -38,6 +46,7 @@ export const createUrqlClient = createClient({
               cache.writeFragment(
                 gql`
                   fragment __ on Post {
+                    id
                     voteCount
                     voteStatus
                   }
@@ -45,6 +54,93 @@ export const createUrqlClient = createClient({
                 { id: postId, voteCount: newCount, voteStatus: value }
               );
             }
+          },
+          createComment: (result, args, cache, _info) => {
+            const {
+              comment: { postId }
+            } = args as CreateCommentMutationVariables;
+            const { comments } = result.createComment as CommentsResponse;
+
+            const data = cache.readFragment(
+              gql`
+                fragment _ on Post {
+                  comments {
+                    id
+                    text
+                    postedBy {
+                      id
+                      firstName
+                      lastName
+                    }
+                    createdAt
+                  }
+                }
+              `,
+              { id: postId }
+            );
+
+            if (data) {
+              const newCommentsList = [comments[0], ...data.comments];
+
+              cache.writeFragment(
+                gql`
+                  fragment __ on Post {
+                    comments {
+                      id
+                      text
+                      postedBy {
+                        id
+                        firstName
+                        lastName
+                      }
+                      createdAt
+                    }
+                  }
+                `,
+                { id: postId, comments: newCommentsList }
+              );
+            }
+          },
+          deleteComment: (_result, args, cache, info) => {
+            const { commentId } = args as DeleteCommentMutationVariables;
+
+            const POST = gql`
+              query Post($id: String!) {
+                post(id: $id) {
+                  post {
+                    id
+                    comments {
+                      id
+                      text
+                      postedBy {
+                        id
+                        firstName
+                        lastName
+                      }
+                      createdAt
+                    }
+                  }
+                }
+              }
+            `;
+
+            cache
+              .inspectFields('Query')
+              .filter(field => field.fieldName === 'post')
+              .forEach(field => {
+                cache.updateQuery(
+                  {
+                    query: POST,
+                    variables: { id: field.arguments.id }
+                  },
+                  data => {
+                    data.post.post.comments = data.post.post.comments.filter(
+                      comment => comment.id !== commentId
+                    );
+                    return data;
+                  }
+                );
+              });
           }
         }
       }
