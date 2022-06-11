5896f719ba68f223c039d350ea04cd8872cbe35a
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-06-10 23:41:10 -0400

    add new font weights

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'urql';
 import '@fontsource/source-sans-pro/400.css';
+import '@fontsource/source-sans-pro/700.css';
 import '@fontsource/open-sans/700.css';
+import '@fontsource/open-sans/600.css';
 import customTheme from 'ui/theme';
 import { AuthProvider } from '../context/auth';
 import { createUrqlClient } from '../utils/createUrqlClient';
