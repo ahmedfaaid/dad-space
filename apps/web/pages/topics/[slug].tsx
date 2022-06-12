8dabc9ce63727e2062fa04bb9652bf28e0d0a415
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2022-06-11 21:51:35 -0400

    Topic pages

--- apps/web/pages/topics/[slug].tsx
+++ apps/web/pages/topics/[slug].tsx
@@ -1,13 +1,38 @@
+import NextLink from 'next/link';
 import { useRouter } from 'next/router';
-import { Heading } from '@chakra-ui/react';
+import { Box, Flex, Link, Spinner } from '@chakra-ui/react';
+import { ArrowBackIcon } from '@chakra-ui/icons';
+import { usePostsByTopicQuery } from 'dad-gql';
 import Layout from '../../components/layout';
+import FeaturedPost from '../../components/FeaturedPost';
 
 export default function SingleTopic() {
   const router = useRouter();
   const { slug } = router.query;
+  const [{ data, fetching }] = usePostsByTopicQuery({
+    variables: {
+      slug: slug as string
+    }
+  });
+
   return (
     <Layout>
-      <Heading>{slug}</Heading>
+      <Box w='80%' mx='auto' mb={8}>
+        <Link onClick={() => router.back()} color='star-command-blue'>
+          <ArrowBackIcon /> Back
+        </Link>
+      </Box>
+      {fetching ? (
+        <Flex align='center' justify='center'>
+          <Spinner />
+        </Flex>
+      ) : (
+        <Box>
+          {data?.postsByTopic.map(post => (
+            <FeaturedPost key={post.id} post={post} />
+          ))}
+        </Box>
+      )}
     </Layout>
   );
 }
